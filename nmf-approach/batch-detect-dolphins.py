b3574d24e4ae16f4b8efd2fa8e45ed6c0683ed1d
rio26 <xqli2626@gmail.com>
2018-11-22 19:50:50 +0800

    fixed the bug for not updating batch size

--- nmf-approach/batch-detect-dolphins.py
+++ nmf-approach/batch-detect-dolphins.py
@@ -14,36 +14,25 @@ _smallnumber = 1E-6
 
 class SNMF():
 
-    # def __init__(self, V, K, alpha, beta, iterations):
-    #     """
-    #     Perform matrix factorization to predict empty
-    #     entries in a matrix.
-
-    #     Arguments
-    #     - V (ndarray)   : user-item rating matrix
-    #     - K (int)       : number of latent dimensions
-    #     - alpha (float) : learning rate
-    #     - beta (float)  : regularization parameter
-    #     """
-
-    #     self.V = V
-    #     self.num_users, self.num_items = V.shape
-    #     self.K = K
-    #     self.alpha = alpha
-    #     self.beta = beta
-    #     self.iterations = iterations
     """
-        x: adjancy matrix (symmetric) with dimension (nxn)
-        h: result matrix with dimention (nxr)
-        r: number of clusters
+    Input:
+      -- V: m x n matrix, the dataset
+
+    Optional Input/Output:
+      -- l: penalty lambda (trade-off parameter between the regularization term and the loss term.)
+    
+      -- w_init: basis matrix with size m x r
+      -- h_init: weight matrix with size r x n  (r is the number of cluster)
+      -- Output: w, h
     """
     def __init__(self, x, h_init = None, r = 2, batch_number = 10):
         self.x = x
         self.r = r
-        self.mini_batch_size = math.ceil(x.shape[0] / 10)
+        self.batch_number = batch_number
+        self.mini_batch_size = math.ceil(x.shape[0] / batch_number)
+        print("Batch number is : ", batch_number, " with mini_batch_size: ", self.mini_batch_size)
         print("the matrix's row and column are: ", self.x.shape[0], self.x.shape[1])
         self.h = h_init
-
 
     def frobenius_norm(self):
         """ Euclidean error between x and h * h.T """
@@ -55,25 +44,26 @@ class SNMF():
         return error
 
     def sgd_solver(self, max_iter = 100):
-        for iter in range(max_iter):
-            lo = random.randint(0, (self.x.shape[0] - self.mini_batch_size - 1))
-            # print("batch index is: ", lo)
-            batch_x = self.x[lo: (lo + self.mini_batch_size), lo: (lo + self.mini_batch_size)].todense()  ## correct
-            # print("batch matrix:", np.shape(batch_x), "and its element: ", batch_x.todense())
-            batch_h = self.h[lo: (lo + self.mini_batch_size), :] ##correct
-            # print("batch_h is: ",  np.shape(batch_h), "and its element: ", batch_h) # dolphin case is (7,7)
-            
+        if(self.batch_number == 1):        # normal MUR
+            for iter in range(max_iter):
+                tmp_x = self.x.todense()
+                self.h = np.multiply(self.h, (np.dot(tmp_x, self.h) / (np.dot(np.dot(self.h, self.h.T) ,self.h) + 2 ** -8)))
+        else:
+            for iter in range(max_iter):  # stochastic MUR         
+                lo = random.randint(0, (self.x.shape[0] - self.mini_batch_size - 1))
+                # print("batch index is: ", lo)
+                batch_x = self.x[lo: (lo + self.mini_batch_size), lo: (lo + self.mini_batch_size)].todense()  ## correct
+                # print("batch matrix:", np.shape(batch_x), "and its element: ", batch_x)
+                batch_h = self.h[lo: (lo + self.mini_batch_size), :] ##correct
+                # print("batch_h is: ",  np.shape(batch_h), "and its element: ", batch_h) # dolphin case is (7,7)
+                
 
-            # self.h = 0.5 * np.multiply(self.h, (1 + (np.dot(self.x.T, self.h) /  (np.dot(np.dot(self.h, self.h.T), self.h) + 2 ** -8))))
-            # tmp = np.multiply(batch_h, ((batch_x, np.dot.(np.dot(batch_h, batch_h.T)) /  (np.dot(np.dot(batch_h, batch_h.T), batch_h)))))
-            # tmp = np.dot(np.dot(batch_x, batch_h), batch_h.T) / np.dot(np.dot(np.dot(batch_h, batch_h.T) ,batch_h) ,batch_h.T)
-            self.h[lo: (lo + self.mini_batch_size), :] = np.multiply(batch_h, (np.dot(batch_x, batch_h) / (np.dot(np.dot(batch_h, batch_h.T) ,batch_h) + 2 ** -8)))
-            # updatedtmp = batch_h * tmp  ## (7,2) (7,7) not match
-            # tmp = np.dot(batch_x, batch_h)
-            # print("Test Tmp shape: ", np.shape(tmp), "and its value: \n", tmp)
+                # self.h = 0.5 * np.multiply(self.h, (1 + (np.dot(self.x.T, self.h) /  (np.dot(np.dot(self.h, self.h.T), self.h) + 2 ** -8))))
+                # tmp = np.multiply(batch_h, ((batch_x, np.dot.(np.dot(batch_h, batch_h.T)) /  (np.dot(np.dot(batch_h, batch_h.T), batch_h)))))
+                # tmp = np.dot(np.dot(batch_x, batch_h), batch_h.T) / np.dot(np.dot(np.dot(batch_h, batch_h.T) ,batch_h) ,batch_h.T)
+                self.h[lo: (lo + self.mini_batch_size), :] = np.multiply(batch_h, (np.dot(batch_x, batch_h) / (np.dot(np.dot(batch_h, batch_h.T) ,batch_h) + 2 ** -8)))
+                # print("Test Tmp shape: ", np.shape(tmp), "and its value: \n", tmp)
  
-        # print("\n \n final round equivalence:", np.array_equal(self.h, self.h[lo: (lo + self.mini_batch_size), :] = tmp))
-
         # print("updated h, with size: ", np.shape(tmp), "; type: ", type(tmp))
         # print("updated h's element: ", np.shape(tmp[0][0]), "; type: ", type(tmp[0][0]), "value: ", tmp[0][0])
         # # xx = self.x.todense()
@@ -92,14 +82,14 @@ cluster_num = 2
 A = nx.adjacency_matrix(G)   #(62，62)                              # get adjacency matrix
 initial_h = np.random.rand(A.shape[0], cluster_num)                 # h's initialization
 grid1 = np.dot(initial_h,initial_h.T)                               # x's initial value
-A_nmf = SNMF(A, r=cluster_num,  h_init = initial_h, batch_number=1) # call snmf's constructor
+A_nmf = SNMF(A, r=cluster_num,  h_init = initial_h, batch_number=3) # call snmf's constructor
 
 
 
 print("Staring error is: ", A_nmf.frobenius_norm())
 print("Start running...")
 t0 = time()
-result = A_nmf.sgd_solver(max_iter=50)                              # run gd, return h
+result = A_nmf.sgd_solver(max_iter=2000)                              # run gd, return h
 t1 = time()
 
 print('Final error is: ', A_nmf.frobenius_norm(), 'Time taken: ', t1 - t0)
