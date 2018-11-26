7a392d397174bd7b7ed6554cb92597152c944378
rio26 <xqli2626@gmail.com>
2018-11-26 17:51:22 +0800

    updated the error trend and find another bug...
    WHY IT CONVERGES SO WEIRDLY!!!

--- nmf-approach/batch-detect-dolphins.py
+++ nmf-approach/batch-detect-dolphins.py
@@ -25,14 +25,16 @@ class SNMF():
       -- h_init: weight matrix with size r x n  (r is the number of cluster)
       -- Output: w, h
     """
-    def __init__(self, x, h_init = None, r = 2, batch_number = 10):
+    def __init__(self, x, h_init = None, r = 2, batch_number = 10, max_iter = 100):
         self.x = x
         self.r = r
         self.batch_number = batch_number
         self.mini_batch_size = math.ceil(x.shape[0] / batch_number)
+        self.max_iter = max_iter
         print("Batch number is : ", batch_number, " with mini_batch_size: ", self.mini_batch_size)
         print("the matrix's row and column are: ", self.x.shape[0], self.x.shape[1])
         self.h = h_init
+        self.errors = np.zeros(self.max_iter)
 
     def frobenius_norm(self):
         """ Euclidean error between x and h * h.T """
@@ -43,13 +45,16 @@ class SNMF():
             error = None
         return error
 
-    def sgd_solver(self, max_iter = 100):
+    def sgd_solver(self):
         if(self.batch_number == 1):        # normal MUR
-            for iter in range(max_iter):
+            for iter in range(self.max_iter):
+                self.errors[iter] = np.linalg.norm(self.x - np.dot(self.h, self.h.T), 'fro') # record error
+
                 tmp_x = self.x.todense()
                 self.h = np.multiply(self.h, (np.dot(tmp_x, self.h) / (np.dot(np.dot(self.h, self.h.T) ,self.h) + 2 ** -8)))
         else:
-            for iter in range(max_iter):  # stochastic MUR         
+            for iter in range(self.max_iter):  # stochastic MUR     
+                self.errors[iter] = np.linalg.norm(self.x - np.dot(self.h, self.h.T), 'fro') # record error
                 lo = random.randint(0, (self.x.shape[0] - self.mini_batch_size - 1))
                 # print("batch index is: ", lo)
                 batch_x = self.x[lo: (lo + self.mini_batch_size), lo: (lo + self.mini_batch_size)].todense()  ## correct
@@ -74,6 +79,9 @@ class SNMF():
         # print(num_viol)
         return self.h
 
+    def get_error_trend(self):
+        return self.errors
+
 # read .gml
 G = nx.read_gml('dolphins-v62-e159/dolphins.gml') # 62 vertices
 cluster_num = 2
@@ -82,18 +90,20 @@ cluster_num = 2
 A = nx.adjacency_matrix(G)   #(62，62)                              # get adjacency matrix
 initial_h = np.random.rand(A.shape[0], cluster_num)                 # h's initialization
 grid1 = np.dot(initial_h,initial_h.T)                               # x's initial value
-A_nmf = SNMF(A, r=cluster_num,  h_init = initial_h, batch_number=3) # call snmf's constructor
+A_nmf = SNMF(A, r=cluster_num,  h_init = initial_h, batch_number=1, max_iter=50) # call snmf's constructor
 
 
 
 print("Staring error is: ", A_nmf.frobenius_norm())
 print("Start running...")
 t0 = time()
-result = A_nmf.sgd_solver(max_iter=2000)                              # run gd, return h
+result = A_nmf.sgd_solver()                              # run gd, return h
 t1 = time()
 
 print('Final error is: ', A_nmf.frobenius_norm(), 'Time taken: ', t1 - t0)
 
+plt.plot(A_nmf.get_error_trend())
+plt.show()
 
 """
 ----------------------------------------PLOT----------------------------------------
